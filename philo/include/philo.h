59f7297469b4774d95f5c0fc78a9d70c5b494c9b
Abusalah0 <109g83@gmail.com>
2025-01-11 14:09:39 +0300

    removed libft flags from makefile, added some libraries

---
+++ philo/include/philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 13:53:54 by abdsalah          #+#    #+#             */
+/*   Updated: 2025/01/11 14:08:58 by abdsalah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <semaphore.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+
+
+#endif
