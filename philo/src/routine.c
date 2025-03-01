89f7a7458bb5f53ddb4fcde7d58da540de17a58e
Abusalah0 <109g83@gmail.com>
2025-03-01 15:04:43 +0300

    thread creation

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 16:08:56 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/24 16:14:50 by ahramada         ###   ########.fr       */
+/*   Updated: 2025/03/01 15:04:15 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <pthread.h>
 
 void *routine(t_philo *philo)
 {
@@ -21,6 +20,5 @@ void *routine(t_philo *philo)
     printf("Hello from routine\n");
     printf("%d time %ld.%06ld\n", philo->number, tv.tv_sec, tv.tv_usec);
     pthread_mutex_unlock(philo->print);
-    pthread_detach(philo->id);
     return (NULL);
 }
--- philo/src/start.c
+++ philo/src/start.c
@@ -3,29 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   start.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahramada <ahramada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 16:07:42 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/02/24 17:25:24 by ahramada         ###   ########.fr       */
+/*   Updated: 2025/03/01 15:04:20 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-int join_philos(t_prog *prog)
-{
-    int i;
-
-    i = 0;
-    while (i < prog->input[NUM_OF_PHILO])
-    {
-        if (pthread_join(prog->philos[i].id, NULL))
-        {
-            return (0);
-        }
-    }
-    return (1);
-}
 
 static int create_philos(t_prog *prog)
 {
@@ -40,14 +25,21 @@ static int create_philos(t_prog *prog)
         }
         i++;
     }
+    i = 0;
+    while (i < prog->input[NUM_OF_PHILO])
+    {
+        if (pthread_join(prog->philos[i].id, NULL))
+        {
+            return (0);
+        }
+        i++;
+    }
     return (1);
 }
 
 int start(t_prog *prog)
 {
     if (!create_philos(prog))
-        return (0);
-    if (!join_philos(prog))
         return (0);
     return (1);
 }
